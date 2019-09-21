82a99ee174c7cf8b226ef145dec9d25ac11d11e6
Craig Coleman <CColeSV07@Gmail.com>
2019-09-20 21:52:10 -0400

    trying this out

--- js/main.js
+++ js/main.js
@@ -1,42 +1,60 @@
-quote = fetch()
+// quote = fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+//     .then(function(response) {
+//         console.log('response.json', response.json());
+//         return response.json();
+//     })
 
 
 const quoteArray = [
-    "This is quote 0",
-    "This is quote 1",
-    "This is quote 2",
-    "This is quote 3"
+    {
+        quote:"This is quote 0",
+        author: "Hakeem Olajuwan"
+    },
+    {
+        quote:"This is quote 1",
+        author: "Hakeem Stewart"
+    },
+    {
+        quote:"This is quote 2",
+        author: "Hakeem Roberts"
+    },
+    {
+        quote:"This is quote 3",
+        author: "Hakeem Johnson"
+    },
+    {
+        quote:"This is quote 4",
+        author: "Hakeem Bravo"
+    },
+    {
+        quote:"This is quote 5",
+        author: "Hakeem Blue"
+    },
+    {
+        quote:"This is quote 6",
+        author: "Hakeem Red"
+    },
 ];
 
-function randomQuote(quoteArray) {
-    return quoteArray[Math.floor(Math.random() * quoteArray.length)];
+let randomText = document.getElementById("text");
+let randomAuthor = document.getElementById("author");
+
+
+
+function randomQuote() {
+    //get random number - 5
+    //change inner html of text  quoteArray[5]
+    //change inner html of author quoteArray[5]
+
+    // return quoteArray[Math.floor(Math.random() * quoteArray.length)];
  }
+  
 
 
 const brandNewText = "This is my brand new text";
 
 function newText() {
-    let randomText = document.getElementById("text"); 
-    randomText.innerHTML = randomQuote;
-    
+    let randomNumber = Math.floor(Math.random() * quoteArray.length);
+    randomText.innerHTML = quoteArray[randomNumber].quote;    
+    randomAuthor.innerHTML = quoteArray[randomNumber].author;
 }
-
-
-
-
-// fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
-//  .then(function(response) {
-//      return response.json();
-//  })
-
-
-/* 
--=Within The Button=-
-Randomize Text      using Math.floor + array  or  api
-Change Background
-
-Use Fetch
-*/
-
-
-console.log("text")
